409ab4154f4c0c5ea00a5175af16c18734012ce0
MatthewAClark <matthewclark86@gmail.com>
2019-03-20 12:09:52 +0000

    Added a get out clause for singing more than 100 ducks, and not allowing negative ducks!

--- src/songs/duckSong.java
+++ src/songs/duckSong.java
@@ -18,35 +18,46 @@ public class duckSong {
         if (ducks == 0) {
             System.out.println("No ducks? - that's a shame, I can't sing my song!");
         }
+        if (ducks < 0) {
+            System.out.println("Please be sensible, no negative ducks allowed");
+        }
         String word = "ducks";
         if ( ducks == 1) {
             word = "duck";
         }
-        for (int i = ducks; i > 0; i--) {
 
-            System.out.println(i + " little "+ word +" went swimming one day");
-            System.out.println("Over the hill and far away");
-            System.out.println("Mother duck said, 'Quack quack quack quack'");
+        if (ducks < 100) {
+            for (int i = ducks; i > 0; i--) {
 
-            if (ducks > 2) {
-                if ( i > 1 ) {
-                    if ( (i-1) == 1) {
-                        word = "duck";
+                System.out.println(i + " little "+ word +" went swimming one day");
+                System.out.println("Over the hill and far away");
+                System.out.println("Mother duck said, 'Quack quack quack quack'");
+
+                if (ducks > 2) {
+                    if ( i > 1 ) {
+                        if ( (i-1) == 1) {
+                            word = "duck";
+                        }
+                        System.out.println("And only " + (i - 1) + " little "+ word + " came back!");
                     }
-                    System.out.println("And only " + (i - 1) + " little "+ word + " came back!");
+                    else
+                    {
+                        System.out.println("And all her "+ducks +" little ducks came back!");
+                    }
                 }
-                else
-                {
-                    System.out.println("And all her "+ducks +" little ducks came back!");
+                if (ducks == 2){
+                    System.out.println("And both her ducks came back!");
                 }
-            }
-            if (ducks == 2){
-                System.out.println("And both her ducks came back!");
-            }
-            if (ducks == 1){
-                System.out.println("And her lonely little duck came back!");
-            }
+                if (ducks == 1){
+                    System.out.println("And her lonely little duck came back!");
+                }
 
+            }
         }
+        else
+        {
+            System.out.println("I'm not singing that much!");
+        }
+
     }
 }
